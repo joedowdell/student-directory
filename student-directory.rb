618a1e544ccc075407f5d3145caa3b565e5816a4
Joe Dowdell <joedowdell@gmail.com>
2014-06-25 14:08:05 +0100

    takes in multiple entries into student array

--- student-directory.rb
+++ student-directory.rb
@@ -2,15 +2,24 @@
 # This is our student array to store students
 students = []
 
+# This is the method to take input of student details
 def student_input(students)
 	puts "Please enter the name of the first student and press RETURN"
 	puts "or press RETURN to exit"
 	name = gets.chomp
-	puts "Please enter the cohort month"
-	cohort = gets.chomp
-	puts "Please enter student's nationality"
-	nationality = gets.chomp
-	students << { name: name, cohort: cohort, nationality: nationality}
+		while !name.empty?
+			puts "Please enter the cohort month"
+			cohort = gets.chomp.capitalize.to_sym
+				if cohort.empty?
+						cohort = :June
+				end 
+			puts "Please enter student's nationality"
+			nationality = gets.chomp
+				
+			students << { name: name, cohort: cohort, nationality: nationality}
+			puts "Please enter name of next student and press RETURN to continue, or press RETURN to exit"
+			name = gets.chomp
+		end
 end
 
 student_input(students)
