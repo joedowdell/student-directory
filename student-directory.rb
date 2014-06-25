618a66a8535f48bc4422024d5e56e92015f5ef8d
Joe Dowdell <joedowdell@gmail.com>
2014-06-25 16:23:47 +0100

    sort by cohort and nationality

--- student-directory.rb
+++ student-directory.rb
@@ -6,7 +6,7 @@ students = []
 def student_input(students)
 	puts "Please enter the name of the first student and press RETURN"
 	puts "or press RETURN to exit"
-	name = gets.chomp
+	name = gets.chomp.capitalize
 		while !name.empty?
 			puts "Please enter the cohort month"
 			cohort = gets.chomp.capitalize.to_sym
@@ -21,7 +21,7 @@ def student_input(students)
 			student_tally(students)
 
 			puts "Please enter name of next student and press RETURN to continue, or press RETURN to exit"
-			name = gets.chomp
+			name = gets.chomp.capitalize
 		end
 		students
 end
@@ -38,6 +38,14 @@ def student_tally(students)
 end
 
 
+# This is a method to sort students by name and cohort
+def sort_cohort(students)
+	students.sort_by! { |student| [student[:cohort], student[:name], student[:nationality]] }
+	print_students(students)
+end
+
+
+
 # This is the method to print cohort infomation to the screen
 def print_students(students)
 	if students.empty? 
@@ -52,9 +60,13 @@ def print_students(students)
 end
 
 student_input(students)
+sort_cohort(students)
+=begin
+student_input(students)
+sort_by_cohort(students)
 print_students(students)
 student_tally(students)
-
+=end
 # puts students
 
 
