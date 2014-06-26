fe74140d805308a4ffc7ea50b0b19faeeb34ad37
Joe Dowdell <joedowdell@gmail.com>
2014-06-26 11:00:53 +0100

    added interactive menu

--- student-directory.rb
+++ student-directory.rb
@@ -1,6 +1,33 @@
+# This is the method for the interactive menu
 
-# This is our student array to store students
-students = []
+def interactive_menu
+	students = []
+	loop do 
+		#1. print the menu and ask the user what to do
+		puts "1. Input the students"
+		puts "2. Show the students"
+		puts "9. Exit"
+		#2. read the input and save it into a variable
+		selection = gets.chomp
+		#3. do what the user has asked
+		case selection
+			when "1"
+				#input the students
+				students = student_input(studentsgit )
+			when "2"
+				#show the students
+				print_students(students)
+				student_tally(students)
+			when "9"
+				exit #terminate the program
+			else 
+				puts "I don't know what you want, try again"
+		end
+
+	end
+end
+
+
 
 # This is the method to take input of student details
 def student_input(students)
@@ -37,37 +64,41 @@ def student_tally(students)
 	
 end
 
-
-# This is a method to sort students by name and cohort
-def sort_cohort(students)
-	students.sort_by! { |student| [student[:cohort], student[:name], student[:nationality]] }
-	print_students(students)
-end
-
-
-
 # This is the method to print cohort infomation to the screen
 def print_students(students)
 	if students.empty? 
 		puts "There are no students in the database!"
 	else
-	puts "This is a list of all students in the Makers Academy database"
+		puts "This is a list of all students in the Makers Academy database"
 		students.each.with_index(1) do |student, index|
 			puts "\t#{index}. #{student[:name]}, #{student[:cohort]}, #{student[:nationality]}"
-		end	
+		end
 	end
-	
 end
 
-student_input(students)
-sort_cohort(students)
+# This is a method to sort students by name and cohort
+def sort_cohort(students)
+	students.sort_by! { |student| [student[:cohort], student[:name], student[:nationality]] }
+	remove_false_students(students)
+end
+
+# This is a method to remove anyone from a cohort that is after than June
+
 =begin
-student_input(students)
-sort_by_cohort(students)
-print_students(students)
-student_tally(students)
+	
+def remove_false_students(students)
+	students = students.delete_if { |student| student[:cohort] == "August" || student[:cohort] ==  "September" || student[:cohort] ==  "October" }
+	students
+end
 =end
-# puts students
+
+interactive_menu
+
+#student_input(students)
+# remove_false_students(students)
+#sort_cohort(students)
+#student_tally(students)
+#puts students
 
 
 
