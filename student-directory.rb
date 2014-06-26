90719f05c44d5056a1f3d714cf0d4fdc88db5461
Joe Dowdell <joedowdell@gmail.com>
2014-06-26 11:39:48 +0100

    refactoring interactive menu

--- student-directory.rb
+++ student-directory.rb
@@ -1,36 +1,43 @@
 # This is the method for the interactive menu
 
 def interactive_menu
-	students = []
-	loop do 
-		#1. print the menu and ask the user what to do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-		#2. read the input and save it into a variable
-		selection = gets.chomp
-		#3. do what the user has asked
-		case selection
-			when "1"
-				#input the students
-				students = student_input(studentsgit )
-			when "2"
-				#show the students
-				print_students(students)
-				student_tally(students)
-			when "9"
-				exit #terminate the program
-			else 
-				puts "I don't know what you want, try again"
-		end
-
+	@students = []
+	loop do
+		print_menu
+		process(gets.chomp)
 	end
 end
 
+# print the menu and ask the user what to do
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
 
+	#show the students
+def show_students
+	print_students
+	student_tally
+end
+
+# do what the user has asked
+def process(selection)
+	case selection
+		when "1"
+			#input the students
+			student_input
+		when "2"
+			show_students
+		when "9"
+			exit #terminate the program
+		else 
+			puts "I don't know what you want, try again"
+	end
+end
 
 # This is the method to take input of student details
-def student_input(students)
+def student_input
 	puts "Please enter the name of the first student and press RETURN"
 	puts "or press RETURN to exit"
 	name = gets.chomp.capitalize
@@ -43,43 +50,43 @@ def student_input(students)
 			puts "Please enter student's nationality"
 			nationality = gets.chomp
 
-			students << { name: name, cohort: cohort, nationality: nationality}
+			@students << { name: name, cohort: cohort, nationality: nationality}
 
-			student_tally(students)
+			student_tally
 
 			puts "Please enter name of next student and press RETURN to continue, or press RETURN to exit"
 			name = gets.chomp.capitalize
 		end
-		students
+		@students
 end
 
 # This method is to taly the number of students
-def student_tally(students)
+def student_tally
 	
-	if students.length == 1
-		puts "There is #{students.length} student in the database"
+	if @students.length == 1
+		puts "There is #{@students.length} student in the database"
 	else
-		puts "There are #{students.length} students in the database"
+		puts "There are #{@students.length} students in the database"
 	end
 	
 end
 
 # This is the method to print cohort infomation to the screen
-def print_students(students)
-	if students.empty? 
+def print_students
+	if @students.empty? 
 		puts "There are no students in the database!"
 	else
 		puts "This is a list of all students in the Makers Academy database"
-		students.each.with_index(1) do |student, index|
+		@students.each.with_index(1) do |student, index|
 			puts "\t#{index}. #{student[:name]}, #{student[:cohort]}, #{student[:nationality]}"
 		end
 	end
 end
 
 # This is a method to sort students by name and cohort
-def sort_cohort(students)
-	students.sort_by! { |student| [student[:cohort], student[:name], student[:nationality]] }
-	remove_false_students(students)
+def sort_cohort
+	@students.sort_by! { |student| [student[:cohort], student[:name], student[:nationality]] }
+	remove_false_students(@students)
 end
 
 # This is a method to remove anyone from a cohort that is after than June
